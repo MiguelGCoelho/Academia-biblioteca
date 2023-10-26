ea048f611f9326eb5beaa9eda0ea5328b00b2267
MiguelGCoelho <72024990+MiguelGCoelho@users.noreply.github.com>
2023-10-26 10:39:05 +0000

    Novo livro

--- academia-biblioteca/src/main/resources/data.sql
+++ academia-biblioteca/src/main/resources/data.sql
@@ -3,3 +3,4 @@ insert into book values (2, 'The Roman Hat Mystery', 'Elery Queen');
 insert into book values (3, 'Maigret e a Velha Senhora', 'Simenon, Georges');
 insert into book values (4, 'Uma Morte Súbita', 'Rowling, J.K.');
 insert into book values (5, 'Crime no Expresso do Oriente', 'Agatha Christie');
+insert into book values 6, 'A vida de Miguel Coelho', 'Miguel Coelho');
